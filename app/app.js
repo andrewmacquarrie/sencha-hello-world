4845e96f78637fbdd950c80e845f68842a321c94
drewmacqu <andrew.macquarrie@gmail.com>
2012-02-25 11:07:03 +0000

    moving app.js into coffeescipt

--- app/app.js
+++ app/app.js
@@ -1,17 +1,27 @@
-Ext.Loader.setConfig({enabled:true});
-Ext.application({
+(function() {
+  Ext.Loader.setConfig({
+    enabled: true
+  });
+  Ext.application({
     name: 'App',
     controllers: ['main'],
     launch: function() {
-  		var panel = Ext.create('Ext.Panel', {
-  		  fullscreen: true,
-  		  layout: {
-  		    type: 'hbox'
-		    },
-  		  items: [
-  		    { xtype: 'mainForm', flex: 1 },
-  		    { xtype: 'nameDisplay', flex: 1 }
-  		  ]
-			});
-		}
-});
+      var panel;
+      return panel = Ext.create('Ext.Panel', {
+        fullscreen: true,
+        layout: {
+          type: 'hbox'
+        },
+        items: [
+          {
+            xtype: 'mainForm',
+            flex: 1
+          }, {
+            xtype: 'nameDisplay',
+            flex: 1
+          }
+        ]
+      });
+    }
+  });
+}).call(this);
